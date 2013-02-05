548b869535d1760678d55f1e79410d68fab4b71d
Jennifer Clark <jac300@nyu.edu>
2013-02-05 12:12:27 -0500

    Initial Commit

---
+++ LikelyTo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LikelyTo
+//
+//  Created by Jennifer Clark on 2/5/13.
+//  Copyright (c) 2013 Jennifer Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LikelyTo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LikelyTo
+//
+//  Created by Jennifer Clark on 2/5/13.
+//  Copyright (c) 2013 Jennifer Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
